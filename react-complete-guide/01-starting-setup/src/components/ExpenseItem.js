a6b2f9a998ec3f4c8218c500480883768924c7b9
truongh10 <haotruong1997@xxx.com>
2023-02-03 22:33:17 -0800

    added some styling to ExpenseItem component

--- react-complete-guide/01-starting-setup/src/components/ExpenseItem.js
+++ react-complete-guide/01-starting-setup/src/components/ExpenseItem.js
@@ -1,6 +1,16 @@
 // this component represent a single expense item
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-  return <h2>Expense Item!</h2>;
+  return (
+    <div className="expense-item">
+      <div>Feb 2nd 2023</div>
+      <div className="expense-item__description">
+        <h2>Food</h2>
+        <div className="expense-item__price">$14.99</div>
+      </div>
+    </div>
+  );
 }
 
 // need to export it, then import it where we need to use it
