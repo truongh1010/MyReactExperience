98672f4300686e7fd8fbbe3959ce2ad1863a1fb7
truongh10 <haotruong1997@xxx.com>
2023-02-03 22:11:17 -0800

    created ExpenseItem component

---
+++ react-complete-guide/01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,7 @@
+// this component represent a single expense item
+function ExpenseItem() {
+  return <h2>Expense Item!</h2>;
+}
+
+// need to export it, then import it where we need to use it
+export default ExpenseItem;
