dee908360b99ffd2506f98152f4b61d4af66bf47
truongh10 <haotruong1997@xxx.com>
2023-02-08 14:23:13 -0800

    set props/properties for ExpenseItem component

--- react-complete-guide/01-starting-setup/src/components/ExpenseItem.js
+++ react-complete-guide/01-starting-setup/src/components/ExpenseItem.js
@@ -1,17 +1,19 @@
 // this component represent a single expense item
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 2, 13);
-  const expenseTitle = "Food";
-  const expenseAmount = 14.99;
+// react function takes one parameter as ann object
+// this object will hold all the values we get for the attributes on custom element
+function ExpenseItem(props) {
+  // const expenseDate = new Date(2022, 2, 13);
+  // const expenseTitle = "Food";
+  // const expenseAmount = 14.99;
 
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
