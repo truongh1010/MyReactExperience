ae1f01f46c850fb5b62aaeb04ecee25bd3aea4dd
truongh10 <haotruong1997@xxx.com>
2023-03-07 09:14:39 -0800

    created adduser form

---
+++ SimpleReactApp/src/components/Users/AddUser.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const AddUser = (props) => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={addUserHandler}>
+      <label htmlFor="username">UserName</label>
+      <input id="username" type="text"></input>
+      <label htmlFor="age">Age</label>
+      <input id="age" type="number"></input>
+      <button type="submit">Add User</button>
+    </form>
+  );
+};
+
+export default AddUser;
