08266b831546aa5932e3ce80f73ad1f4c106f3bc
truongh10 <haotruong1997@xxx.com>
2023-03-07 09:35:58 -0800

    created customed buttom component

--- SimpleReactApp/src/components/Users/AddUser.js
+++ SimpleReactApp/src/components/Users/AddUser.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 
@@ -15,7 +16,7 @@ const AddUser = (props) => {
         <input id="username" type="text"></input>
         <label htmlFor="age">Age</label>
         <input id="age" type="number"></input>
-        <button type="submit">Add User</button>
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
